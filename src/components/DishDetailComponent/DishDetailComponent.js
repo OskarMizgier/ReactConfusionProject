632d679c58e01985dccc4165babd8f5c4200d6a2
Oskar Mizgier <o.mizgier@gmail.com>
2019-11-02 08:44:19 +0100

    Added the functionality to render corresponding comments

--- src/components/DishDetailComponent/DishDetailComponent.js
+++ src/components/DishDetailComponent/DishDetailComponent.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle, } from 'reactstrap';
 import DISHES from '../Shared/dishes';
+import './DishDetail.css';
 
 
 class DishDetail extends Component {
@@ -12,24 +13,53 @@ constructor(props) {
 
 
 
-render () {
-              if(this.props.selectedDish != null) {
-            return (
-            <div className='row'>
-            <div className='col-12 col-md-5'>
+
+renderDish () {
+    if(this.props.selectedDish != null) {
+        return (
+        <div className='row'>
+        <div className='col-12 col-md-5'>
+        <Card>
+            <CardImg src={this.props.selectedDish.image} alt='' />
+            <CardBody>
+                <CardTitle>{this.props.selectedDish.name}</CardTitle>
+                <CardText>{this.props.selectedDish.description}</CardText>
+            </CardBody>
+        </Card>
+        </div>
+        <div id='CommentClass' className='col-12 col-md-5'>
             <Card>
-                <CardImg src={this.props.selectedDish.image} alt='' />
-                <CardBody>
-                    <CardTitle>{this.props.selectedDish.name}</CardTitle>
-                    <CardText>{this.props.selectedDish.description}</CardText>
-                </CardBody>
+            <CardBody>
+                <CardTitle>Comments:</CardTitle>
+                    {   this.props.selectedDish.comments.map((comment) => {
+                        return <ul key={comment.id} className='list-unstyled'>
+                                <li>{comment.author + ' ' + comment.date.substring(0, 10) }</li>
+                                 <li>{comment.comment}</li>
+                                    </ul>
+                    })}
+            </CardBody>
+
             </Card>
-            </div>
-            </div>
-                )
-        } else 
-            return <div>
-                 </div>
+
+
+        </div>
+          
+        </div>
+            )
+    } else 
+        return <div>
+             </div>
+}
+
+
+render () {
+            return (this.renderDish(this.props.selectedDish)
+        
+            )
+
+
+           
+                 
     }
     
 }
