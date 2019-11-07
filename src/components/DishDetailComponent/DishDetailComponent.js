1cb55bf05b728833e36a76ff2e45332bb825191e
Oskar Mizgier <o.mizgier@gmail.com>
2019-11-07 19:52:54 +0100

    Added a post comment modal to the DishDetailComponent with simple redux control and validation

--- src/components/DishDetailComponent/DishDetailComponent.js
+++ src/components/DishDetailComponent/DishDetailComponent.js
@@ -1,19 +1,37 @@
 import React, { Component } from 'react';
-import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle, } from 'reactstrap';
+import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle, Modal, ModalBody, ModalHeader, Form, FormGroup, Label, Row, Col } from 'reactstrap';
 import DISHES from '../Shared/dishes';
 import './DishDetail.css';
 import { Link } from 'react-router-dom';
 import { Breadcrumb, BreadcrumbItem, } from 'reactstrap'
+import { Control, LocalForm, Errors } from 'react-redux-form';
+
+
+const maxLength = (len) => (val) => !(val) || (val.length <= len);
+const minLength = (len) => (val) => val && (val.length >= len);
 
 
 class DishDetail extends Component {
 constructor(props) {
     super(props)
-
+    this.state = {
+        isModalOpen: false
+    }
+    this.toggleModal = this.toggleModal.bind(this);
 }
 
 
+toggleModal() {
+    this.setState({
+      isModalOpen: !this.state.isModalOpen
+    });
+  }
 
+  handleSubmit(values) {
+    console.log('Current State is: ' + JSON.stringify(values));
+    alert('Current State is: ' + JSON.stringify(values));
+    // event.preventDefault();
+}
 
 
 renderDish () {
@@ -51,8 +69,63 @@ renderDish () {
                                  <li>{comment.comment}</li>
                                     </ul>
                     })}
+                     <button className='btn btn-primary' onClick={this.toggleModal}><span   >&#9829; Add a comment</span></button>
             </CardBody>
-
+            
+                 <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+                    <ModalHeader toggle={this.toggleModal}>Leave your comment</ModalHeader>
+                    <ModalBody>
+                    <LocalForm onSubmit={(values) => this.handleSubmit(values)}>
+                    {/* Row 1 */}
+                    <Row className="form-group">
+                                <Col>
+                                    <Control.select model=".contactType" name="contactType"
+                                        className="form-control">
+                                        <option>Good</option>
+                                        <option>Very Good</option>
+                                        <option>Excellent</option>
+                                        <option>Outstanding</option>
+                                    </Control.select>
+                                </Col>
+                            </Row>
+                     {/* Row 2 */}
+                     <Row className="form-group">
+                                <Col>
+                                    <Control.text model=".name" id="name" name="name"
+                                        placeholder="Your Name"
+                                        className="form-control"
+                                        validators={{
+                                            minLength: minLength(3), maxLength: maxLength(15)
+                                        }}
+                                         />
+                                    {/* Error for the name */}
+                                    <Errors
+                                        className="text-danger"
+                                        model=".name"
+                                        show="touched"
+                                        messages={{
+                                            minLength: 'Must be greater than 2 characters',
+                                            maxLength: 'Must be 15 characters or less'
+                                        }}
+                                     />
+                                </Col>
+                            </Row>
+                            {/* Row 3 */}
+                            <Row className="form-group">
+                                <Col>
+                                    <Control.textarea model=".comment" id="comment" name="comment"
+                                        rows="12" placeholder='Please type your comment'
+                                        className="form-control" />
+                                </Col>
+                                </Row>
+                                <Row>
+                                    <Col className='justify-content-center'> 
+                                    <button type='submit' className='btn-primary'>Post your comment</button>
+                                    </Col>
+                            </Row>
+                        </LocalForm>
+                    </ModalBody>
+                </Modal>
             </Card>
 
 
